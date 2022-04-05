123ad941c4555382a475d114f7439473673fd289
Rupert Pople <rupertpople@gmail.com>
2022-04-05 17:04:09 +0100

    Initalized files to make Battle

---
+++ app.rb
@@ -0,0 +1,15 @@
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+class Battle < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  # our routes would go here
+get '/' do
+    p 'Hello Battle'
+end
+  # # Start the server if this file is executed directly (do not change the line below)
+  run! if app_file == $0
+end
